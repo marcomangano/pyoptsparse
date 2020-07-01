0b218d33b0bae8b539383d07d3f5896d47b8ea7e
marcomangano <mmangano@umich.edu>
2020-07-01 12:22:50 -0400

    updated slsqpb

--- pyoptsparse/pySLSQP/source/slsqpb.f
+++ pyoptsparse/pySLSQP/source/slsqpb.f
@@ -1,8 +1,8 @@
-      SUBROUTINE SLSQPB (M, MEQ, LA, N, X, XL, XU, F, C, G, A, ACC,
-     *                   ITER, MODE, R, L, X0, MU, S, U, V, W, IW,
-     *                   ALPHA, F0, GS, H1, H2, H3, H4, T, T0, TOL,
-     *                   IEXACT, INCONS, IRESET, ITERMX, LINE, N1, 
-     *                   N2, N3)
+      SUBROUTINE SLSQPB (m, meq, la, n, x, xl, xu, f, c, g, a, acc,
+     *                   iter, mode, r, l, x0, mu, s, u, v, w, iw,
+     *                   alpha, f0, gs, h1, h2, h3, h4, t, t0, tol,
+     *                   iexact, incons, ireset, itermx, line, 
+     *                   n1, n2, n3)
 
 C   NONLINEAR PROGRAMMING BY SOLVING SEQUENTIALLY QUADRATIC PROGRAMS
 
@@ -10,222 +10,245 @@ C        -  L1 - LINE SEARCH,  POSITIVE DEFINITE  BFGS UPDATE  -
 
 C                      BODY SUBROUTINE FOR SLSQP
 
-      INTEGER          IW(*), I, IEXACT, INCONS, IRESET, ITER, ITERMX,
-     *                 K, J, LA, LINE, M, MEQ, MODE, N, N1, N2, N3
+      INTEGER          iw(*), i, iexact, incons, ireset, iter, itermx,
+     *                 k, j, la, line, m, meq, mode, n, n1, n2, n3
+      LOGICAL          badlin
 
-      DOUBLE PRECISION A(LA,N+1), C(LA), G(N+1), L((N+1)*(N+2)/2),
-     *                 MU(LA), R(M+N+N+2), S(N+1), U(N+1), V(N+1), W(*),
-     *                 X(N), XL(N), XU(N), X0(N),
+      DOUBLE PRECISION a(la,n+1), c(la), g(n+1), l((n+1)*(n+2)/2),
+     *                 mu(la), r(m+n+n+2), s(n+1), u(n+1), v(n+1), w(*),
+     *                 x(n), xl(n), xu(n), x0(n),
      *                 DDOT, DNRM2, LINMIN,
-     *                 ACC, ALFMIN, ALPHA, F, F0, GS, H1, H2, H3, H4,
-     *                 HUN, ONE, T, T0, TEN, TOL, TWO, ZERO
+     *                 acc, alfmin, alpha, f, f0, gs, h1, h2, h3, h4,
+     *                 hun, one, t, t0, ten, tol, two, ZERO
 
 c     dim(W) =         N1*(N1+1) + MEQ*(N1+1) + MINEQ*(N1+1)  for LSQ
 c                     +(N1-MEQ+1)*(MINEQ+2) + 2*MINEQ
 c                     +(N1+MINEQ)*(N1-MEQ) + 2*MEQ + N1       for LSEI
 c                      with MINEQ = M - MEQ + 2*N1  &  N1 = N+1
 
-      DATA             ZERO /0.0D0/, ONE /1.0D0/, ALFMIN /1.0D-1/,
-     *                 HUN /1.0D+2/, TEN /1.0D+1/, TWO /2.0D0/
+      DATA             ZERO /0.0d0/, one /1.0d0/, alfmin /1.0d-1/,
+     *                 hun /1.0d+2/, ten /1.0d+1/, two /2.0d0/
 
-      IF (MODE) 260, 100, 220
+C     The badlin flag keeps track whether the SQP problem on the current
+C     iteration was inconsistent or not.
+      badlin = .false.
 
-  100 ITERMX = ITER
-      IF (ACC.GE.ZERO) THEN
-          IEXACT = 0
+      IF (mode) 260, 100, 220
+
+  100 itermx = iter
+      IF (acc.GE.ZERO) THEN
+          iexact = 0
       ELSE
-          IEXACT = 1
+          iexact = 1
       ENDIF
-      ACC = ABS(ACC)
-      TOL = TEN*ACC
-      ITER = 0
-      IRESET = 0
-      N1 = N + 1
-      N2 = N1*N/2
-      N3 = N2 + 1
-      S(1) = ZERO
-      MU(1) = ZERO
-      CALL DCOPY(N, S(1),  0, S,  1)
-      CALL DCOPY(M, MU(1), 0, MU, 1)
+      acc = ABS(acc)
+      tol = ten*acc
+      iter = 0
+      ireset = 0
+      n1 = n + 1
+      n2 = n1*n/2
+      n3 = n2 + 1
+      s(1) = ZERO
+      mu(1) = ZERO
+      CALL DCOPY(n, s(1),  0, s,  1)
+      CALL DCOPY(m, mu(1), 0, mu, 1)
 
 C   RESET BFGS MATRIX
 
-  110 IRESET = IRESET + 1
-      IF (IRESET.GT.5) GO TO 255
-      L(1) = ZERO
-      CALL DCOPY(N2, L(1), 0, L, 1)
-      J = 1
-      DO 120 I=1,N
-         L(J) = ONE
-         J = J + N1 - I
+  110 ireset = ireset + 1
+      IF (ireset.GT.5) GO TO 255
+      l(1) = ZERO
+      CALL DCOPY(n2, l(1), 0, l, 1)
+      j = 1
+      DO 120 i=1,n
+         l(j) = one
+         j = j + n1 - i
   120 CONTINUE
 
 C   MAIN ITERATION : SEARCH DIRECTION, STEPLENGTH, LDL'-UPDATE
 
-  130 ITER = ITER + 1
-      MODE = 9
-      IF (ITER.GT.ITERMX) GO TO 330
+  130 iter = iter + 1
+      mode = 9
+      IF (iter.GT.itermx) GO TO 330
 
 C   SEARCH DIRECTION AS SOLUTION OF QP - SUBPROBLEM
 
-      CALL DCOPY(N, XL, 1, U, 1)
-      CALL DCOPY(N, XU, 1, V, 1)
-      CALL DAXPY(N, -ONE, X, 1, U, 1)
-      CALL DAXPY(N, -ONE, X, 1, V, 1)
-      H4 = ONE
-      CALL LSQ (M, MEQ, N , N3, LA, L, G, A, C, U, V, S, R, W, IW, MODE)
+      CALL DCOPY(n, xl, 1, u, 1)
+      CALL DCOPY(n, xu, 1, v, 1)
+      CALL DAXPY(n, -one, x, 1, u, 1)
+      CALL DAXPY(n, -one, x, 1, v, 1)
+      h4 = one
+      CALL LSQ (m, meq, n , n3, la, l, g, a, c, u, v, s, r, w, iw, mode)
 
 C   AUGMENTED PROBLEM FOR INCONSISTENT LINEARIZATION
+C
+C   If it turns out that the original SQP problem is inconsistent,
+C   disallow termination with convergence on this iteration,
+C   even if the augmented problem was solved.
 
-      IF (MODE.EQ.6) THEN
-          IF (N.EQ.MEQ) THEN
-              MODE = 4
+      badlin = .false.
+      IF (mode.EQ.6) THEN
+          IF (n.EQ.meq) THEN
+              mode = 4
           ENDIF
       ENDIF
-      IF (MODE.EQ.4) THEN
-          DO 140 J=1,M
-             IF (J.LE.MEQ) THEN
-                 A(J,N1) = -C(J)
+      IF (mode.EQ.4) THEN
+          badlin = .true.
+          DO 140 j=1,m
+             IF (j.LE.meq) THEN
+                 a(j,n1) = -c(j)
              ELSE
-                 A(J,N1) = MAX(-C(J),ZERO)
+                 a(j,n1) = MAX(-c(j),ZERO)
              ENDIF
   140     CONTINUE
-          S(1) = ZERO
-          CALL DCOPY(N, S(1), 0, S, 1)
-          H3 = ZERO
-          G(N1) = ZERO
-          L(N3) = HUN
-          S(N1) = ONE
-          U(N1) = ZERO
-          V(N1) = ONE
-          INCONS = 0
-  150     CALL LSQ (M, MEQ, N1, N3, LA, L, G, A, C, U, V, S, R,
-     *              W, IW, MODE)
-          H4 = ONE - S(N1)
-          IF (MODE.EQ.4) THEN
-              L(N3) = TEN*L(N3)
-              INCONS = INCONS + 1
-              IF (INCONS.GT.5) GO TO 330
+          s(1) = ZERO
+          CALL DCOPY(n, s(1), 0, s, 1)
+          h3 = ZERO
+          g(n1) = ZERO
+          l(n3) = hun
+          s(n1) = one
+          u(n1) = ZERO
+          v(n1) = one
+          incons = 0
+  150     CALL LSQ (m, meq, n1, n3, la, l, g, a, c, u, v, s, r,
+     *              w, iw, mode)
+          h4 = one - s(n1)
+          IF (mode.EQ.4) THEN
+              l(n3) = ten*l(n3)
+              incons = incons + 1
+              IF (incons.GT.5) GO TO 330
               GOTO 150
-          ELSE IF (MODE.NE.1) THEN
+          ELSE IF (mode.NE.1) THEN
               GOTO 330
           ENDIF
-      ELSE IF (MODE.NE.1) THEN
+      ELSE IF (mode.NE.1) THEN
           GOTO 330
       ENDIF
 
 C   UPDATE MULTIPLIERS FOR L1-TEST
 
-      DO 160 I=1,N
-         V(I) = G(I) - DDOT(M,A(1,I),1,R,1)
+      DO 160 i=1,n
+         v(i) = g(i) - DDOT(m,a(1,i),1,r,1)
   160 CONTINUE
-      F0 = F
-      CALL DCOPY(N, X, 1, X0, 1)
-      GS = DDOT(N, G, 1, S, 1)
-      H1 = ABS(GS)
-      H2 = ZERO
-      DO 170 J=1,M
-         IF (J.LE.MEQ) THEN
-             H3 = C(J)
+      f0 = f
+      CALL DCOPY(n, x, 1, x0, 1)
+      gs = DDOT(n, g, 1, s, 1)
+      h1 = ABS(gs)
+      h2 = ZERO
+      DO 170 j=1,m
+         IF (j.LE.meq) THEN
+             h3 = c(j)
          ELSE
-             H3 = ZERO
+             h3 = ZERO
          ENDIF
-         H2 = H2 + MAX(-C(J),H3)
-         H3 = ABS(R(J))
-         MU(J) = MAX(H3,(MU(J)+H3)/TWO)
-         H1 = H1 + H3*ABS(C(J))
+         h2 = h2 + MAX(-c(j),h3)
+         h3 = ABS(r(j))
+         mu(j) = MAX(h3,(mu(j)+h3)/two)
+         h1 = h1 + h3*ABS(c(j))
   170 CONTINUE
 
 C   CHECK CONVERGENCE
 
-      MODE = 0
-      IF (H1.LT.ACC .AND. H2.LT.ACC) GO TO 330
-      H1 = ZERO
-      DO 180 J=1,M
-         IF (J.LE.MEQ) THEN
-             H3 = C(J)
+      mode = 0
+      IF (h1.LT.acc .AND. h2.LT.acc .AND. .NOT. badlin
+     *     .AND. f .EQ. f) GO TO 330
+      h1 = ZERO
+      DO 180 j=1,m
+         IF (j.LE.meq) THEN
+             h3 = c(j)
          ELSE
-             H3 = ZERO
+             h3 = ZERO
          ENDIF
-         H1 = H1 + MU(J)*MAX(-C(J),H3)
+         h1 = h1 + mu(j)*MAX(-c(j),h3)
   180 CONTINUE
-      T0 = F + H1
-      H3 = GS - H1*H4
-      MODE = 8
-      IF (H3.GE.ZERO) GO TO 110
+      t0 = f + h1
+      h3 = gs - h1*h4
+      mode = 8
+      IF (h3.GE.ZERO) GO TO 110
 
 C   LINE SEARCH WITH AN L1-TESTFUNCTION
 
-      LINE = 0
-      ALPHA = ONE
-      IF (IEXACT.EQ.1) GOTO 210
+      line = 0
+      alpha = one
+      IF (iexact.EQ.1) GOTO 210
 
 C   INEXACT LINESEARCH
 
-  190     LINE = LINE + 1
-          H3 = ALPHA*H3
-          CALL DSCAL(N, ALPHA, S, 1)
-          CALL DCOPY(N, X0, 1, X, 1)
-          CALL DAXPY(N, ONE, S, 1, X, 1)
-          MODE = 1
+  190     line = line + 1
+          h3 = alpha*h3
+          CALL DSCAL(n, alpha, s, 1)
+          CALL DCOPY(n, x0, 1, x, 1)
+          CALL DAXPY(n, one, s, 1, x, 1)
+          mode = 1
           GO TO 330
-  200         IF (H1.LE.H3/TEN .OR. LINE.GT.10) GO TO 240
-              ALPHA = MAX(H3/(TWO*(H3-H1)),ALFMIN)
+  200         IF (h1.LE.h3/ten .OR. line.GT.10) GO TO 240
+              alpha = MAX(h3/(two*(h3-h1)),alfmin)
               GO TO 190
 
 C   EXACT LINESEARCH
 
-  210 IF (LINE.NE.3) THEN
-          ALPHA = LINMIN(LINE,ALFMIN,ONE,T,TOL)
-          CALL DCOPY(N, X0, 1, X, 1)
-          CALL DAXPY(N, ALPHA, S, 1, X, 1)
-          MODE = 1
+  210 IF (line.NE.3) THEN
+          alpha = LINMIN(line,alfmin,one,t,tol)
+          CALL DCOPY(n, x0, 1, x, 1)
+          CALL DAXPY(n, alpha, s, 1, x, 1)
+          mode = 1
           GOTO 330
       ENDIF
-      CALL DSCAL(N, ALPHA, S, 1)
+      CALL DSCAL(n, alpha, s, 1)
       GOTO 240
 
 C   CALL FUNCTIONS AT CURRENT X
 
-  220     T = F
-          DO 230 J=1,M
-             IF (J.LE.MEQ) THEN
-                 H1 = C(J)
+  220     t = f
+          DO 230 j=1,m
+             IF (j.LE.meq) THEN
+                 h1 = c(j)
              ELSE
-                 H1 = ZERO
+                 h1 = ZERO
              ENDIF
-             T = T + MU(J)*MAX(-C(J),H1)
+             t = t + mu(j)*MAX(-c(j),h1)
   230     CONTINUE
-          H1 = T - T0
-          GOTO (200, 210) IEXACT+1
+          h1 = t - t0
+          GOTO (200, 210) iexact+1
 
 C   CHECK CONVERGENCE
 
-  240 H3 = ZERO
-      DO 250 J=1,M
-         IF (J.LE.MEQ) THEN
-             H1 = C(J)
+  240 h3 = ZERO
+      DO 250 j=1,m
+         IF (j.LE.meq) THEN
+             h1 = c(j)
          ELSE
-             H1 = ZERO
+             h1 = ZERO
          ENDIF
-         H3 = H3 + MAX(-C(J),H1)
+         h3 = h3 + MAX(-c(j),h1)
   250 CONTINUE
-      IF ((ABS(F-F0).LT.ACC .OR. DNRM2(N,S,1).LT.ACC) .AND. H3.LT.ACC)
+      IF ((ABS(f-f0).LT.acc .OR. DNRM2(n,s,1).LT.acc) .AND. h3.LT.acc
+     *     .AND. .NOT. badlin .AND. f .EQ. f)
      *   THEN
-            MODE = 0
+            mode = 0
          ELSE
-            MODE = -1
+            mode = -1
          ENDIF
       GO TO 330
 
 C   CHECK relaxed CONVERGENCE in case of positive directional derivative
 
   255 CONTINUE
-      IF ((ABS(F-F0).LT.TOL .OR. DNRM2(N,S,1).LT.TOL) .AND. H3.LT.TOL)
-     *   THEN
-            MODE = 0
+      h3 = ZERO
+      DO 256 j=1,m
+         IF (j.LE.meq) THEN
+             h1 = c(j)
          ELSE
-            MODE = 8
+             h1 = ZERO
+         ENDIF
+         h3 = h3 + MAX(-c(j),h1)
+  256 CONTINUE
+      IF ((ABS(f-f0).LT.tol .OR. DNRM2(n,s,1).LT.tol) .AND. h3.LT.tol
+     *     .AND. .NOT. badlin .AND. f .EQ. f)
+     *   THEN
+            mode = 0
+         ELSE
+            mode = 8
          ENDIF
       GO TO 330
 
@@ -233,54 +256,58 @@ C   CALL JACOBIAN AT CURRENT X
 
 C   UPDATE CHOLESKY-FACTORS OF HESSIAN MATRIX BY MODIFIED BFGS FORMULA
 
-  260 DO 270 I=1,N
-         U(I) = G(I) - DDOT(M,A(1,I),1,R,1) - V(I)
+  260 DO 270 i=1,n
+         u(i) = g(i) - DDOT(m,a(1,i),1,r,1) - v(i)
   270 CONTINUE
 
 C   L'*S
 
-      K = 0
-      DO 290 I=1,N
-         H1 = ZERO
-         K = K + 1
-         DO 280 J=I+1,N
-            K = K + 1
-            H1 = H1 + L(K)*S(J)
+      k = 0
+      DO 290 i=1,n
+         h1 = ZERO
+         k = k + 1
+         DO 280 j=i+1,n
+            k = k + 1
+            h1 = h1 + l(k)*s(j)
   280    CONTINUE
-         V(I) = S(I) + H1
+         v(i) = s(i) + h1
   290 CONTINUE
 
 C   D*L'*S
 
-      K = 1
-      DO 300 I=1,N
-         V(I) = L(K)*V(I)
-         K = K + N1 - I
+      k = 1
+      DO 300 i=1,n
+         v(i) = l(k)*v(i)
+         k = k + n1 - i
   300 CONTINUE
 
 C   L*D*L'*S
 
-      DO 320 I=N,1,-1
-         H1 = ZERO
-         K = I
-         DO 310 J=1,I - 1
-            H1 = H1 + L(K)*V(J)
-            K = K + N - J
+      DO 320 i=n,1,-1
+         h1 = ZERO
+         k = i
+         DO 310 j=1,i - 1
+            h1 = h1 + l(k)*v(j)
+            k = k + n - j
   310    CONTINUE
-         V(I) = V(I) + H1
+         v(i) = v(i) + h1
   320 CONTINUE
 
-      H1 = DDOT(N,S,1,U,1)
-      H2 = DDOT(N,S,1,V,1)
-      H3 = 0.2D0*H2
-      IF (H1.LT.H3) THEN
-          H4 = (H2-H3)/(H2-H1)
-          H1 = H3
-          CALL DSCAL(N, H4, U, 1)
-          CALL DAXPY(N, ONE-H4, V, 1, U, 1)
+      h1 = DDOT(n,s,1,u,1)
+      h2 = DDOT(n,s,1,v,1)
+      h3 = 0.2d0*h2
+      IF (h1.LT.h3) THEN
+          h4 = (h2-h3)/(h2-h1)
+          h1 = h3
+          CALL DSCAL(n, h4, u, 1)
+          CALL DAXPY(n, one-h4, v, 1, u, 1)
       ENDIF
-      CALL LDL(N, L, U, +ONE/H1, V)
-      CALL LDL(N, L, V, -ONE/H2, U)
+      IF (h1.EQ.0 .or. h2.EQ.0) THEN
+C         Singular update: reset hessian.
+          GO TO 110
+      end if
+      CALL ldl(n, l, u, +one/h1, v)
+      CALL ldl(n, l, v, -one/h2, u)
 
 C   END OF MAIN ITERATION
 
